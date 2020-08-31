c66f99cd85f07a86aeea53ef461c15715ab5a62d
Balashev Konstantin <balashev.konstantin@nicetu.spb.ru>
2020-08-31 14:26:44 +0300

    line width, time

--- three-selection.js
+++ three-selection.js
@@ -2,20 +2,19 @@ class ThreeSelection {
 
     constructor() {
         this.selectedObjects = [];
-        this.selectedObjects.background = null;
         this.overrideMaterial = new THREE.MeshBasicMaterial({
             color: new THREE.Color(0xffffff)
         });
         this.renderTarget = new THREE.WebGLRenderTarget(2048, 2048);
-
+        this.uniforms = {
+            tex: new THREE.Uniform(this.renderTarget.texture),
+            height: new THREE.Uniform(1),
+            width: new THREE.Uniform(1),
+            time: new THREE.Uniform(0),
+        }
         const quadGeometry = new THREE.PlaneGeometry(2, 2);
         const shaderMaterial = new THREE.ShaderMaterial({
-            uniforms: {
-                tex: {
-                    type: "t",
-                    value: this.renderTarget.texture
-                }
-            },
+            uniforms: this.uniforms,
             transparent: true,
             vertexShader: `
                 varying vec2 vUv;
@@ -27,23 +26,28 @@ class ThreeSelection {
             fragmentShader: `
                 varying vec2 vUv;
                 uniform sampler2D tex;
+                uniform float width;
+                uniform float height;
+                uniform float time;
+                
+                const float Pi2 = 6.28318530718; 
+                const float Directions = 16.0;
+                const float Quality = 3.5; 
+                const float Size = 8.0; 
                 
                 void main() {
-                    float Pi2 = 6.28318530718; 
-                    float Directions = 16.0;
-                    float Quality = 3.0; 
-                    float Size = 8.0; 
-                    vec2 Radius = vec2(0.005);
-
-                    vec4 Color = texture2D(tex, vUv);
+        
+                    vec2 Radius = Size/vec2(width, height);
+                    float alpha = texture2D(tex, vUv).a;
                     
                     for (float d = 0.0; d < Pi2; d += Pi2/Directions) {
                         for (float i = 1.0/Quality; i <= 1.0; i += 1.0/Quality) {
-                            Color += texture2D( tex, vUv+vec2(cos(d),sin(d))*Radius*i);
+                            alpha += texture2D( tex, vUv+vec2(cos(d),sin(d))*Radius*i).a;
                         }
                     }
-             
-                    gl_FragColor = vec4(1.,0.,0.,1.)*min(vec4(1.0), Color) - Color / (Quality * Directions - 15.0);
+                    float result = alpha / (Quality * Directions - 15.0);
+                    vec4 outlineColor = vec4(1.0, sin(time)*0.5+0.5, 0.0, 1.0); 
+                    gl_FragColor = outlineColor * (min(1.0, alpha) - result);
                 }
             `
         });
@@ -78,6 +82,9 @@ class ThreeSelection {
 
         const autoClear = renderer.autoClear;
         renderer.autoClear = false;
+        this.uniforms.width.value = innerWidth;
+        this.uniforms.height.value = innerHeight;
+        this.uniforms.time.value = Date.now()%10000/1000;
         renderer.render(this.fullScreenQuadScene, this.orthographicCamera);
         renderer.autoClear = autoClear;
 
